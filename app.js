b3dc681dd675638252dfed9d8ba84c2eba65e4fe
Antonio Navarro <anavarro67@hotmail.com>
2024-12-29 14:27:54 +0100

    feat: Crear estructura para modelo vista-controldaor:  tarifas y calendario
    Se crea el modelo vista-contralador de las colecciones: tarifas y calendario
     Se incluyen las rutas de reservas, tarifas y calendario.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const  express = require('express');
 const morgan = require('morgan')
 const clienteRoutes = require('./src/routes/clientes.route')
 const servicesRoutes = require('./src/routes/services.route')
+const ratesRoutes = require('./src/routes/rates.route')
 
+const multer = require('multer');
+const AWS = require('aws-sdk');
 const app = express();
+
 
 // Middleware para convertir a json los retornos de las peticiones. 
 app.use(express.json())
@@ -21,8 +25,12 @@ app.use( (req, res, next)  => {
     next()
 })
 
+
 app.use('/clientes', clienteRoutes)
+app.use('/reservas', servicesRoutes)
 app.use('/servicios', servicesRoutes)
+app.use('/tarifas', ratesRoutes)
+app.use('/calendario', servicesRoutes)
 
 // Se crea peticion .get de prueba para ver si funciona. 
 app.get('/', (req, res)=> {
